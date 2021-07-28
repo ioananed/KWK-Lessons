e6bc588d12fb090a1be1dca202a9d8c0b67bc8f5
Ioana <33571434+ioananed@users.noreply.github.com>
2021-07-28 20:59:58 +0200

    Miniproyect

---
+++ miniproyect/Actions&outlets/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Actions&outlets
+//
+//  Created by Ioana Nedelcu on 28/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var appTitle: UILabel!
+  
+ 
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+        }
+    }
+
+    
+    
+
+
