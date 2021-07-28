c431646299a414555a011b75462d1daf38838ee1
Ioana <33571434+ioananed@users.noreply.github.com>
2021-07-28 17:44:48 +0200

    First commit

---
+++ StoryBoardintro/StoryBoardintro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryBoardintro
+//
+//  Created by Ioana Nedelcu on 28/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
