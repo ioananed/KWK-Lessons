2c8d991bd76a4a87c6214787c80eaa40ef189633
Ioana <33571434+ioananed@users.noreply.github.com>
2021-07-29 18:12:19 +0200

    Segues

---
+++ KWK/segues/segues/secondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  secondViewcontrollerViewController.swift
+//  segues
+//
+//  Created by Ioana Nedelcu on 29/7/21.
+//
+
+import UIKit
+
+class secondViewcontrollerViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
